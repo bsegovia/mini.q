c30c59e30f2e71bb3b7808f2cbbe31a6d66c73a7
Ben Segovia <segovia.benjamin@gmail.com>
2014-02-10 15:40:20 +0000

    added missing files

--- data/shaders/deferred_fp.glsl
+++ data/shaders/deferred_fp.glsl
@@ -1,8 +1,18 @@
 //##const char deferred_fp[] = {
 void main() {
-  vec2 uv = floor(gl_FragCoord.xy);
+  vec2 uv = gl_FragCoord.xy;
   vec3 nor = normalize(2.0*texture2DRect(u_nortex, uv).xyz-1.0);
-  SWITCH_WEBGL(gl_FragColor, rt_col) = vec4(nor,1.0);
+  float depth = texture2DRect(u_depthtex, uv).r;
+  vec4 outcol = vec4(0.0);
+  if (depth != 1.0) {
+    vec4 posw = u_invmvp * vec4(uv, depth, 1.0);
+    vec3 pos = posw.xyz / posw.w;
+    vec3 ldir = u_lightpos-pos;
+    float llen2 = dot(ldir,ldir);
+    outcol.xyz = vec3(2000.0) * max(dot(nor,ldir),0.0) / (llen2*llen2);
+    outcol.w = 1.0;
+  }
+  SWITCH_WEBGL(gl_FragColor, rt_col) = outcol;
 }
 //##};
 
