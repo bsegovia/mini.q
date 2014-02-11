cb68a33feb153a8618ece78a97d774884ff81970
Ben Segovia <segovia.benjamin@gmail.com>
2014-02-11 21:55:53 +0000

    implemented a cleaner way to instantiate shaders but less macros

--- shaderdecl.hpp
+++ shaderdecl.hpp
@@ -4,7 +4,30 @@
  -------------------------------------------------------------------------*/
 
 /*-------------------------------------------------------------------------
- - we define our macro system here...
+ - first we define a shadertype with all the given uniforms
+ -------------------------------------------------------------------------*/
+#ifndef SHADERNAMESPACE
+#define SHADERNAMESPACE SHADERNAME
+#endif
+#define VATTRIB(T,N,LOC)
+#define FRAGDATA(T,N,LOC)
+#define UNIFORM(T,N) u32 N;
+#define UNIFORMI(T,N,I) u32 N;
+#define INCLUDE(N)
+namespace SHADERNAMESPACE {
+struct shadertype {
+#include VERTEX_PROGRAM
+#include FRAGMENT_PROGRAM
+};
+#undef INCLUDE
+#undef UNIFORMI
+#undef UNIFORM
+#undef VATTRIB
+#undef FRAGDATA
+} /* namespace SHADERNAMESPACE */
+
+/*-------------------------------------------------------------------------
+ - we instantiate the shaderdescriptor here
  -------------------------------------------------------------------------*/
 #if !defined(__WEBGL__)
 #define FRAGDATA(T,N,LOC)\
@@ -22,51 +45,17 @@
 #define VATTRIB(T,N,LOC)\
   static const shaders::attribdesc N##desc(&attrib,LOC,#N,#T);
 
-#define BEGIN_SHADER(SHADER, NAMESPACE) namespace NAMESPACE {\
-  static ogl::shadertype shader;\
-  static const char vppath[] = "data/shaders/" STRINGIFY(SHADER) "_vp.glsl";\
-  static const char fppath[] = "data/shaders/" STRINGIFY(SHADER) "_fp.glsl";\
-  static const char *vp = shaders:: JOIN(SHADER,_vp);\
-  static const char *fp = shaders:: JOIN(SHADER,_fp);\
-  static vector<shaders::fragdatadesc> *fragdata = NULL;\
-  static vector<shaders::attribdesc> *attrib = NULL;\
-  static vector<shaders::uniformdesc> *uniform = NULL;\
-  static vector<shaders::includedesc> *include = NULL;
+namespace SHADERNAMESPACE {
+static ogl::shadertype shader;
+static const char vppath[] = "data/shaders/" STRINGIFY(SHADERNAME) "_vp.glsl";
+static const char fppath[] = "data/shaders/" STRINGIFY(SHADERNAME) "_fp.glsl";
+static const char *vp = shaders:: JOIN(SHADERNAME,_vp);
+static const char *fp = shaders:: JOIN(SHADERNAME,_fp);
+static vector<shaders::fragdatadesc> *fragdata = NULL;
+static vector<shaders::attribdesc> *attrib = NULL;
+static vector<shaders::uniformdesc> *uniform = NULL;
+static vector<shaders::includedesc> *include = NULL;
 
-#if defined(RELEASE)
-#define DESTROY_SHADER
-#else
-#define DESTROY_SHADER\
-  static void destroy() {\
-    SAFE_DEL(attrib);\
-    SAFE_DEL(fragdata);\
-    SAFE_DEL(uniform);\
-    SAFE_DEL(include);\
-  }\
-  static const shaders::destroyregister destroyreg(destroy);
-#endif
-
-#define END_SHADER(N)\
-  static void destroy() {\
-    SAFE_DEL(attrib);\
-    SAFE_DEL(fragdata);\
-    SAFE_DEL(uniform);\
-    SAFE_DEL(include);\
-  }\
-  static const shaders::destroyregister destroyreg(destroy);\
-  static const shaders::shaderdesc rsc = {\
-    vppath, fppath, vp, fp, &uniform, &attrib, &fragdata, &include, RULES\
-  };\
-  static const shaders::shaderregister shaderreg(shader,rsc,STRINGIFY(N));\
-}
-#ifndef SHADERNAMESPACE
-#define SHADERNAMESPACE SHADERNAME
-#endif
-
-/*-------------------------------------------------------------------------
- - ... and we instantiate the shader descriptor here
- -------------------------------------------------------------------------*/
-BEGIN_SHADER(SHADERNAME, SHADERNAMESPACE)
 #define INCLUDE(N) PINCLUDE(N,true)
 #define UNIFORM(T,N) PUNIFORM(T,N,true)
 #define UNIFORMI(T,N,X) PUNIFORMI(T,N,X,true)
@@ -82,10 +71,22 @@ BEGIN_SHADER(SHADERNAME, SHADERNAMESPACE)
 #undef INCLUDE
 #undef UNIFORM
 #undef UNIFORMI
-END_SHADER(SHADERNAME)
+
+static void destroy() {
+  SAFE_DEL(attrib);
+  SAFE_DEL(fragdata);
+  SAFE_DEL(uniform);
+  SAFE_DEL(include);
+}
+static const shaders::destroyregister destroyreg(destroy);
+static const shaders::shaderdesc rsc = {
+  vppath, fppath, vp, fp, &uniform, &attrib, &fragdata, &include, RULES\
+};
+static const shaders::shaderregister shaderreg(shader,rsc,STRINGIFY(N));
+} /* namespace SHADERNAMESPACE */
 
 /*-------------------------------------------------------------------------
- - finally, clean up this definition hell
+ - finally, we clean up this definition hell
  -------------------------------------------------------------------------*/
 #undef SHADERNAMESPACE
 #undef SHADERNAME
